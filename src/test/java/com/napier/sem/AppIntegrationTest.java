1075a80ec9597c44fc26468eea2b07c1dc110d66
michael mackenzie <michaelmackenzie98@gmail.com>
2023-04-14 15:05:22 +0100

    added integration tests for population report

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -3,6 +3,7 @@ package com.napier.sem;
 import com.napier.sem.businessObjects.reports.CityReport;
 import com.napier.sem.businessObjects.reports.CountryReport;
 import com.napier.sem.businessObjects.reports.LanguageReport;
+import com.napier.sem.businessObjects.reports.PopulationReport;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -162,6 +163,43 @@ public class AppIntegrationTest
     /*
     PopulationReport integration tests
      */
+    @Test
+    void ExecutePopulationQueryNullTest(){
+        PopulationReport.printPopulation(PopulationReport.ExecuteQuery(null, null, null));
+    }
+    @Test
+    void ExecutePopulationQueryNullQueryTest(){
+        PopulationReport.printPopulation(PopulationReport.ExecuteQuery(App.con, null, null));
+    }
+    @Test
+    void ExecutePopulationQueryNullConTest(){
+        PopulationReport.printPopulation(PopulationReport.ExecuteQuery(null, PopulationReport.getContinentPopulation("Europe"), "Europe"));
+    }
+    @Test
+    void ExecutePopulationQueryNullLocationTest(){
+        PopulationReport.printPopulation(PopulationReport.ExecuteQuery(App.con,  PopulationReport.getContinentPopulation("Europe"), null));
+    }
 
+    @Test
+    void ExecutePopulationQueryTest(){
+        PopulationReport.printPopulation(PopulationReport.ExecuteQuery(App.con,  PopulationReport.getContinentPopulation("Europe"), "Europe"));
+    }
+
+    @Test
+    void ExecuteSimpleQueryNullTest(){
+        System.out.println(PopulationReport.ExecuteQuery(null, null));
+    }
+    @Test
+    void ExecuteSimpleQueryNullQueryTest(){
+        System.out.println(PopulationReport.ExecuteQuery(App.con, null));
+    }
+    @Test
+    void ExecuteSimpleQueryNullConTest(){
+        System.out.println(PopulationReport.ExecuteQuery(null, PopulationReport.getWorldPop()));
+    }
+    @Test
+    void ExecuteSimpleQueryTest(){
+        System.out.println(PopulationReport.ExecuteQuery(App.con, PopulationReport.getWorldPop()));
+    }
 }
 
